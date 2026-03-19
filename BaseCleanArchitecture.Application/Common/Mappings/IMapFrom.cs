7505bbab4c8ba5f15e53e6af63045a6036f18992
Nguyen Duc Kien <kienchickenz@gmail.com>
2026-03-19 18:41:53 +0700

    Minor

--- BaseCleanArchitecture.Application/Common/Mappings/IMapFrom.cs
+++
@@ -1,8 +0,0 @@
-using AutoMapper;
-
-namespace BaseCleanArchitecture.Application.Common.Mappings;
-
-public interface IMapFrom<T>
-{
-    void Mapping(Profile profile) => profile.CreateMap(typeof(T), GetType()).ReverseMap();
-}
